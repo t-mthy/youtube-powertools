fa6afaa19f2bb56edd61e31068f363a66a28ed22
t-mthy <100193172+t-mthy@users.noreply.github.com>
2023-11-19 00:41:52 -0600

    Loops only once for now

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,9 +1,39 @@
 console.log('YouTube PowerTools loaded! (^_^)v');
 
+// Function to control video playback
+function controlVideoPlayback(videoId, command, startTime, endTime) {
+  const videoElement = document.querySelector('video');
+  if (!videoElement) return;
+
+  switch (command) {
+    case 'startLoop':
+      videoElement.currentTime = startTime;
+      videoElement.play();
+      const loop = () => {
+        if (videoElement.currentTime >= endTime) {
+          videoElement.currentTime = startTime;
+        }
+        requestAnimationFrame(loop);
+      };
+      loop();
+      break;
+    case 'stopLoop':
+      videoElement.pause();
+      break;
+  }
+}
+
 // Listens for message from React, extracts vid ID from URL, sends it back
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.message === 'GetYoutubeVideoId') {
     const videoId = window.location.search.split('v=')[1]?.split('&')[0];
     sendResponse({ videoId: videoId });
+  } else if (request.message === 'ControlVideoPlayback') {
+    controlVideoPlayback(
+      request.videoId,
+      request.command,
+      request.startTime,
+      request.endTime
+    );
   }
 });
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Box, Card, CardMedia } from '@mui/material';
+import { Checkbox, FormControlLabel, Slider, TextField } from '@mui/material';
 
 // Active tab ID to communicate with new window
 const getTabIdFromURL = () => {
   const queryParams = new URLSearchParams(window.location.search);
   return queryParams.get('tabId');
+};
+
+// Utility functions to convert time
+const convertTimeToSeconds = (timeString) => {
+  const [minutes, seconds] = timeString.split(':').map(Number);
+  return minutes * 60 + seconds;
+};
+
+const convertSecondsToTime = (seconds) => {
+  const minutes = Math.floor(seconds / 60);
+  const secs = seconds % 60;
+  return `${minutes.toString().padStart(2, '0')}:${secs
+    .toString()
+    .padStart(2, '0')}`;
 };
 
 const App = () => {
@@ -14,6 +29,10 @@ const App = () => {
 
   // States
   const [thumbnail, setThumbnail] = useState('');
+  const [loopEnabled, setLoopEnabled] = useState(false);
+  const [loopRange, setLoopRange] = useState([0, 100]); // Example range, replace with video duration
+  const [startTime, setStartTime] = useState('00:00');
+  const [endTime, setEndTime] = useState('00:00'); // Replace with video duration
 
   // Video details
   const fetchVideoDetails = async (videoId) => {
@@ -27,6 +46,51 @@ const App = () => {
     } catch (error) {
       console.error('Error fetching video details:', error);
     }
+  };
+
+  // Send command to content script
+  const sendCommandToContentScript = (command) => {
+    chrome.tabs.sendMessage(tabId, {
+      message: 'ControlVideoPlayback',
+      command: command,
+      startTime: convertTimeToSeconds(startTime),
+      endTime: convertTimeToSeconds(endTime),
+    });
+  };
+
+  // Toggle loop on/off
+  const toggleLoop = (event) => {
+    setLoopEnabled(event.target.checked);
+    if (event.target.checked) {
+      sendCommandToContentScript('startLoop');
+    } else {
+      sendCommandToContentScript('stopLoop');
+    }
+  };
+
+  // Handle slider change
+  const handleSliderChange = (event, newValue) => {
+    setLoopRange(newValue);
+    setStartTime(convertSecondsToTime(newValue[0]));
+    setEndTime(convertSecondsToTime(newValue[1]));
+  };
+
+  // Handle manual time input
+  const handleTimeInput = (event, isStart) => {
+    if (isStart) {
+      setStartTime(event.target.value);
+    } else {
+      setEndTime(event.target.value);
+    }
+    setLoopRange([
+      convertTimeToSeconds(startTime),
+      convertTimeToSeconds(endTime),
+    ]);
+  };
+
+  // Handle double-click on input box
+  const handleDoubleClick = () => {
+    // Logic to get current video timestamp and update input box
   };
 
   // Mount
@@ -46,6 +110,7 @@ const App = () => {
 
   return (
     <Box sx={{ padding: 2 }}>
+      {/* Thumbnail display */}
       {thumbnail && (
         <Card>
           <CardMedia
@@ -56,6 +121,39 @@ const App = () => {
           />
         </Card>
       )}
+
+      {/* Loop Checkbox */}
+      <FormControlLabel
+        control={<Checkbox checked={loopEnabled} onChange={toggleLoop} />}
+        label="Enable Loop"
+      />
+
+      {/* Time Range Slider */}
+      <Slider
+        value={loopRange}
+        onChange={handleSliderChange}
+        valueLabelDisplay="auto"
+        min={0}
+        max={100} // Replace with video duration
+      />
+
+      {/* Time Input Boxes */}
+      <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+        <TextField
+          label="Start Time"
+          value={startTime}
+          onChange={handleTimeInput}
+          onDoubleClick={handleDoubleClick}
+          sx={{ width: '45%' }}
+        />
+        <TextField
+          label="End Time"
+          value={endTime}
+          onChange={handleTimeInput}
+          onDoubleClick={handleDoubleClick}
+          sx={{ width: '45%' }}
+        />
+      </Box>
     </Box>
   );
 };
